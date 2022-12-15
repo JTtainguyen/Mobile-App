a4c0e23f519cdf9e7284aaeca0d380c8184089a6
JTtainguyen <nguyentaidhsp@gmail.com>
2022-12-16 00:19:11 +0700

    Add newsfeed and pÃost screen

--- app/src/main/java/vn/edu/usth/facebookclone/PostActivity.java
+++ app/src/main/java/vn/edu/usth/facebookclone/PostActivity.java
@@ -1,22 +1,25 @@
 package vn.edu.usth.facebookclone;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.widget.Button;
-import android.view.View;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class SignUp extends AppCompatActivity {
-    Button button;
+public class PostActivity extends AppCompatActivity {
+    ImageView closeBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
-        button = findViewById(R.id.signin);
-        button.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_post);
+
+        closeBtn = findViewById(R.id.close);
+
+        closeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SignUp.this, MainActivity.class);
+                Intent intent = new Intent(PostActivity.this, NewFeed.class);
                 startActivity(intent);
             }
         });
