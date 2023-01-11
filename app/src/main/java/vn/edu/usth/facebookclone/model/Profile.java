a54096ca6a34b3c1c32f61268380b14cf4856682
JTtainguyen <nguyentaidhsp@gmail.com>
2023-01-11 12:20:15 +0700

    Add feature

--- app/src/main/java/vn/edu/usth/facebookclone/model/Profile.java
+++ app/src/main/java/vn/edu/usth/facebookclone/model/Profile.java
@@ -6,17 +6,56 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import vn.edu.usth.facebookclone.R;
+import vn.edu.usth.facebookclone.api.UserApi;
 
 public class Profile extends AppCompatActivity {
 
     ImageView backbtn;
+    TextView profileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        profileName = findViewById(R.id.profileName);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://63bbc48ccf99234bfa638851.mockapi.io")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        UserApi userApi = retrofit.create(UserApi.class);
+
+        Call<List<User>> call = userApi.getUsers();
+
+        call.enqueue(new Callback<List<User>>() {
+            @Override
+            public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                if (response.code() != 200) {
+                    return;
+                }
+
+                List<User> users = response.body();
+
+                for (User user : users) {
+                    profileName.setText(user.getFirstName().toString() + user.getLastName().toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<User>> call, Throwable t) {
+            }
+        });
 
         backbtn = findViewById(R.id.back);
 
